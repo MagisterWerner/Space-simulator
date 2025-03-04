5ede59060f3107911b0e5520a6a7b9ecaaa447e1
Tomas Jelf Werner <tomas.jelf.werner@gmail.com>
2025-03-04 23:44:28 +0100

    Final strip

--- main.gd
+++ main.gd
@@ -10,11 +10,6 @@ func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	player_ship.position = screen_size / 2
 	camera.position = player_ship.position
-	
-	player_ship.connect("player_destroyed", _on_player_destroyed)
 
 func _process(_delta: float) -> void:
 	camera.position = player_ship.position
-
-func _on_player_destroyed() -> void:
-	get_tree().create_timer(2.0).timeout.connect(func(): get_tree().reload_current_scene())
